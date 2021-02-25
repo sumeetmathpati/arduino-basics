0158ecc9a0dfc7dec89a80ad233d96a12d9b9903
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-02-25 12:38:58 +0000

    update

---
+++ temperature_and_humidity_sensor/temperature_and_humidity_sensor.ino
@@ -0,0 +1,39 @@
+#include <SimpleDHT.h>
+
+int pinDHT11 = 8;
+SimpleDHT11 dht11;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+void loop()
+{
+  Serial.println("=================================");
+  Serial.println("Sample DHT11...");
+
+  byte temperature = 0;
+  byte humidity = 0;
+  byte data[40] = {0};
+  if (dht11.read(pinDHT11, &temperature, &humidity, data))
+  {
+    Serial.print("Read DHT11 failed");
+    return;
+  }
+  Serial.print("Sample RAW Bits: ");
+  for (int i = 0; i < 40; i++)
+  {
+    Serial.print((int)data[i]);
+    if (i > 0 && ((i + 1) % 4) == 0)
+    {
+      Serial.print(' ');
+    }
+  }
+  Serial.println("");
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature);
+  Serial.print(" *C, ");
+  Serial.print((int)humidity);
+  Serial.println(" %");
+  delay(1000);
+}
